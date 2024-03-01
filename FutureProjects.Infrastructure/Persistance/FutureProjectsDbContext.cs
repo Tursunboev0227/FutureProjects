853abbd87fe683fc91d8e441f4f1ba829977e340
Tursunboev0227 <hayotillotursunboev7@gmail.com>
2024-03-01 08:59:28 +0500

    without error

--- FutureProjects.Infrastructure/Persistance/FutureProjectsDbContext.cs
+++ FutureProjects.Infrastructure/Persistance/FutureProjectsDbContext.cs
@@ -1,11 +1,6 @@
 using DOT.DOMEN.Entities.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace FutureProjects.Infrastructure.Persistance
 {
